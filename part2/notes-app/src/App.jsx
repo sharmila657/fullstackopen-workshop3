1e3b6b84eccf5da7df2eab2283f6d761551141b5
sharmila675 <sharmilajirel8@gmail.com>
2023-12-04 14:34:37 +0545

    part2-b component

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,18 +1,29 @@
-import Note from "./components/Note";
+import { useState } from 'react'
+import Note from './components/Note'
 
-const App = ({ notes }) => {
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
 
   return (
     <div>
       <h1>Notes</h1>
-
       <ul>
-      {notes.map(note => 
+        {notes.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
 }
 
-export default App
+export default App 
