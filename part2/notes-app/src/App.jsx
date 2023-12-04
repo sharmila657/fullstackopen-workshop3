fce8a03966073de7ea41dd575cd33dd87751f140
sharmila675 <sharmilajirel8@gmail.com>
2023-12-05 00:17:12 +0545

    workshop part2-c completed

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
 import Note from './components/Note'
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([]);
+
   const [newNote, setNewNote] = useState(
     'a new note...'
   )
   const [showAll, setShowAll] = useState(true)
+
+
+  useEffect(() => {
+    console.log("hello");
+    //1. get data from backend server
+    let myAxiosPromise = axios.get("http://localhost:3001/notes");
+    myAxiosPromise.then((myResult) => {
+      console.log("returned promise");
+      console.dir(myResult.data);
+      //2. put the data into notes state
+      setNotes(myResult.data);
+    });
+
+    console.log(myAxiosPromise);
+  }, []);
+
+  
 
 
   const notesToShow = showAll
@@ -32,6 +52,7 @@ const App = (props) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+
   return (
     <div>
       <h1>Notes</h1>
