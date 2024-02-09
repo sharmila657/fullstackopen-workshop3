3e252952684a48b4ecf9abedbaca6c35bc6614c3
sharmila675 <sharmilajirel8@gmail.com>
2024-02-09 17:48:40 +0545

    added reducer

--- part6/counter-app/src/main.jsx
+++ part6/counter-app/src/main.jsx
@@ -1,7 +1,39 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import  ReactDOM  from 'react-dom/client'
+import counterReducer from './reducer'
+import { createStore } from 'redux'
 
-const container = document.getElementById("root")
-const root = createRoot(container)
+const store = createStore(counterReducer)
 
-root.render(<App/>)
+const App = () => {
+  const clickButton = (event) => {
+    const dispacthType = event.target.name.toUpperCase();
+    store.dispatch({
+      type: dispacthType
+    })
+  }
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <button onClick={(event)=>clickButton(event)} name='good'>good</button>
+      <button onClick={(event) => clickButton(event)} name='bad'>bad</button>
+      <button onClick={(event)=>clickButton(event)} name='ok'>ok</button>
+
+      <div>good {store.getState().good}</div>
+      <div>bad {store.getState().bad}</div>
+      <div>ok {store.getState().ok}</div>
+
+    </div>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+const renderApp = () => {
+  root.render(<App />)
+}
+renderApp()
+store.subscribe(renderApp)
+
