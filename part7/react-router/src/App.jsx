27cdad5fbc7b8a3d006036bee801fe0001ea8b97
sharmila657 <you@example.com>
2024-02-27 10:43:10 +0545

    workshop using reactrouter

--- part7/react-router/src/App.jsx
+++ part7/react-router/src/App.jsx
@@ -1,5 +1,45 @@
+import {
+  BrowserRouter,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+const Notes =()=>{
+ return 1
+}
+
+const Users =()=>{
+  return 2
+}
+
+const Home =()=>{
+  return 3
+}
+
 const App = () => {
-  return <div>Hello there</div>;
-};
+
+  const padding = {
+    padding: 5
+  }
+
+  return (
+    <BrowserRouter>
+      <div>
+        <Link style={padding} to="/home">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2023</i>
+      </div>
+    </BrowserRouter>
+  )
+}
 
 export default App;
